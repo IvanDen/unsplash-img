255431ba75bdd0851390b181fbc77b9cbd3cefc0
IvanDen <denisko.iv90@gmail.com>
2019-10-31 17:43:54 +0400

    Main component, item img added.

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -7,10 +7,36 @@ const Main = (props) => {
             <h1>Image loader</h1>
             <div className={`${Class.imagesContainer}`}>
                 <div className={`${Class.imgItem}`}>
-                    <img src="img/photo-1562.jpg" alt="pic1"/>
-                </div>
-                <div className={`${Class.imgItem}`}>
-                    <img src="img/photo-1562.jpg" alt="pic1"/>
+                    <figure>
+                        <div className={`${Class.linkWrap}`} >
+                            <a className={Class.linkImg} title={"Some title"} href="/">
+                                <div className={`${Class.imageWrap}`}>
+                                    <img src="img/photo-15725.jpg" alt="pic1"/>
+                                </div>
+                            </a>
+                            <div className={`${Class.infoWrap}`}>
+                                <div className={`${Class.background}`}></div>
+                                <div className={`${Class.usePanel} ${Class.panelTop}`}>
+                                    <div className={`${Class.likeCollect}`}>
+                                        <button title="Like photo" className={`${Class.like}`}>Like</button>
+                                        <button title="Add to collection" className={`${Class.collect}`}>Collect</button>
+                                    </div>
+                                </div>
+                                <div className={`${Class.usePanel} ${Class.panelBottom}`}>
+                                    <div className={`${Class.authorInfo}`}>
+                                        <div className={`${Class.authorInfo}`}>
+                                            <span className={`${Class.authorName}`}>
+                                                <a href="/@hubblecontacts">username</a>
+                                            </span>
+                                            <span className={`${Class.authorProf}`}>
+                                                <a href="/@hubblecontacts">userprofile</a>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </figure>
                 </div>
             </div>
         </div>
